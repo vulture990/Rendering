02196d262a27241d76c5c284bb41248d89dd2f61
Mohamedsalbi <73004418+vulture990@users.noreply.github.com>
2021-02-12 18:05:27 +0100

    Create SpriteSheet.java

---
+++ SpriteSheet.java
@@ -0,0 +1,14 @@
+package com.vulture;
+
+import java.awt.image.BufferedImage;
+
+public class SpriteSheet {
+    public int width ,height;
+    public int[] pixels;
+    public  SpriteSheet(BufferedImage image){
+        width=image.getWidth();
+        height=image.getHeight();
+        pixels = image.getRGB(0,0,width,height,null,0,width);
+
+    }
+}
